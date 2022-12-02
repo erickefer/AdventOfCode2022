d3f6a4dc11c8eddbc5894b03bde9d3df5ba4114b
erickefer <kefer.eric@googlemail.com>
2022-12-02 13:27:13 +0100

    initial commit

---
+++ day1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countElfs = 0;
+            int currentElf = 0;
+            string inputLine;
+            int elfSumCalories = 0;
+            using (StreamReader reader = new StreamReader("input.txt")){
+                while((inputLine = reader.ReadLine()) != null){
+                    if (inputLine == ""){
+                        //Console.WriteLine("Leere Zeile");
+                    } else {
+                        //Console.WriteLine(inputLine);
+                        countElfs++;
+                    }
+                }
+            }
+            int[] elfCalories = new int[countElfs];
+            using (StreamReader reader = new StreamReader("input.txt")){
+                while((inputLine = reader.ReadLine()) != null){
+                    if (inputLine == ""){
+                        //Console.WriteLine("Leere Zeile");
+                        elfCalories[currentElf] = elfSumCalories;
+                        //Console.WriteLine($"Added {elfSumCalories} calories to elf {currentElf}");
+                        elfSumCalories = 0;
+                        currentElf++;
+                    } else {
+                        //Console.WriteLine(inputLine);
+                        elfSumCalories = elfSumCalories + Int32.Parse(inputLine);
+                    }
+                }
+            }
+
+            int topElf = getMostCalories(elfCalories);
+            int secondElf = getMostCalories(elfCalories);
+            int thirdElf = getMostCalories(elfCalories);
+            Console.WriteLine($"Der Elf {topElf} trägt die meisten Kalorien");
+            Console.WriteLine($"Der Elf {secondElf} trägt die meisten Kalorien");
+            Console.WriteLine($"Der Elf {thirdElf} trägt die meisten Kalorien");
+            int sum = topElf + secondElf + thirdElf;
+            Console.WriteLine(sum);
+        }
+
+        public static int getMostCalories(int[] caloriesArray){
+            //Console.WriteLine("Hallo Welt!");
+            int mostCalories = 0;
+            int elfMostCalories = 0;
+            for (int i = 0; i<caloriesArray.Length; i++){
+                if (caloriesArray[i] > mostCalories){
+                    elfMostCalories = i+1;
+                    mostCalories = caloriesArray[i];
+                    //Console.WriteLine(i);
+                }
+            }
+            caloriesArray[elfMostCalories-1] = 0;
+            return mostCalories;
+        }
+    }
+}
---
+++ day2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//main program
+/*
+- Rock beats Sciccors, Paper beats Rock, Sciccors beats Paper 
+- first column is what opponent is playing
+- A = Rock, B = Paper, C = Scissors
+- second column response
+- X = Loss, Y = Draw, Z = Win
+- 1 point for Rock, 2 points for Paper, 3 points for Scissors
+- 0 point for loss, 3 points for draw, 6 points for win
+*/
+Dictionary<string, int> dict = new Dictionary<string, int>();
+dict["AX"] = 3; // Loss 
+dict["AY"] = 4; // Draw
+dict["AZ"] = 8; // Win
+dict["BX"] = 1; // Loss
+dict["BY"] = 5; // Draw
+dict["BZ"] = 9; // Win
+dict["CX"] = 2; // Loss
+dict["CY"] = 6; // Draw
+dict["CZ"] = 7; // Win
+
+using (StreamReader reader = new StreamReader("input.txt")){
+    string line;
+    int totalScore = 0;
+    while ((line = reader.ReadLine()) != null){
+        var lineSplit = line.Split(" ");
+        string opponent = lineSplit[0];
+        string own = lineSplit[1];
+        //Console.WriteLine($"Opponent: {opponent}, Own: {own}");
+        //if (own == "X") totalScore += 1;
+        //if (own == "Y") totalScore += 2;
+        //if (own == "Z") totalScore += 3;
+        string dictEntry = opponent+own;
+        totalScore += dict[dictEntry];
+    }
+    Console.WriteLine(totalScore);
+}
+
+//Methods
