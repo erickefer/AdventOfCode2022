c1be20a3458bc4e50270a7a0fb3df5a80c765078
erickefer <97392413+erickefer@users.noreply.github.com>
2022-12-09 22:30:00 +0100

    part one and two day6

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,2 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using(StreamReader reader = new StreamReader("input.txt")){
+    //firstPart(reader.ReadLine());
+    secondPart(reader.ReadLine(),14);
+}
+
+void firstPart(string input){
+    int resultPosition = 0;
+    for (int i = 0; i<input.Length-4;i++){
+        string subString = input.Substring(i,4);
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        bool containsDuplicate = false;
+        for(int k = 0; k<subString.Length; k++){
+            if(dict.ContainsKey(subString[k])){
+                containsDuplicate = true;
+            } else {
+                dict[subString[k]] = 1;
+            }
+        }
+        if (!containsDuplicate){
+            resultPosition = i+4;
+            i = input.Length;
+        }
+    }
+    Console.WriteLine(resultPosition);
+}
+
+void secondPart(string input, int markerLength){
+    int resultPosition = 0;
+    for (int i = 0; i<input.Length-markerLength;i++){
+        string subString = input.Substring(i,markerLength);
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        bool containsDuplicate = false;
+        for(int k = 0; k<subString.Length; k++){
+            if(dict.ContainsKey(subString[k])){
+                containsDuplicate = true;
+            } else {
+                dict[subString[k]] = 1;
+            }
+        }
+        if (!containsDuplicate){
+            resultPosition = i+markerLength;
+            i = input.Length;
+        }
+    }
+    Console.WriteLine(resultPosition);
+}
